34ff584ecf980e9b45927c96abef1c3122204335
tess hsu <tess.hsu@gmail.com>
2025-04-21 17:35:22 +0200

    Update

--- python_scan/crackme.py
+++ python_scan/crackme.py
@@ -2,12 +2,12 @@ import codecs
 # "le code secret est cachÃ©, personnne ne peut le dÃ©chiffrer"
 code_secret = "plorevav{synt}"
 flag = codecs.decode(code_secret, 'rot_13')
+
 # correspondance de rÃ©fÃ©rence pour le codage/dÃ©codage
 alphabet = str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
    'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm')
 
 def decode_secret(secret):
-	# Fonction de dÃ©codage ROT13
 	return secret.translate(alphabet)
 
 
@@ -21,8 +21,8 @@ def login():
 		if mdp == decode_secret(code_secret):
 			print("ConnectÃ© avec succÃ¨s !")
 		else :
-			print(decode_secret("plorevav{synt}"))
+            print(decode_secret("plorevav{synt}"))
 	else :
-		print("Nom d'utilisateur invalide.")
+		print("Invalide ")
 
 login()
